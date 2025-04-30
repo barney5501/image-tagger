471a79901a5d9f0e6ae87936a3bb7f71d16cc507
Itay <Itaybn100@gmail.com>
2025-04-30 16:10:04 +0300

    basic web tagger interface (HTML)

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -6,3 +6,8 @@ app = Flask(__name__)
 @app.route("/")
 def main():
     return "Image Tagger!"
+
+
+@app.route("/tag")
+def tagger():
+    return render_template("imagetagger.html")

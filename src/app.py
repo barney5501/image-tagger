6965329e53953acc9ad4f222fe5da1f0eaadeb7b
Itay <Itaybn100@gmail.com>
2025-05-01 13:42:27 +0300

    working basic images browsing

--- src/app.py
+++ src/app.py
@@ -1,4 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+import os
+
+imagesPath = "static/images"
+imagesList = os.listdir(imagesPath)
+imagesList = [img for img in imagesList if not img.endswith(".txt")]
+imageIndex = 0
+
 
 app = Flask(__name__)
 
@@ -10,4 +17,29 @@ def main():
 
 @app.route("/tag")
 def tagger():
-    return render_template("imagetagger.html")
+    firstImage = imagesList[0]
+    return render_template(
+        "imagetagger.html", imagesPath=imagesPath, currentImage=firstImage
+    )
+
+
+@app.route("/nextImage")
+def next():
+    global imageIndex
+    if imageIndex != (len(imagesList) - 1):
+        next_image = jsonify(imagesList[imageIndex + 1])
+        imageIndex += 1
+        return next_image
+    else:
+        return jsonify(imagesList[imageIndex])
+
+
+@app.route("/prevImage")
+def previousImage():
+    global imageIndex
+    if imageIndex != 0:
+        previous_image = jsonify(imagesList[imageIndex - 1])
+        imageIndex -= 1
+        return previous_image
+    else:
+        return jsonify(imagesList[imageIndex])

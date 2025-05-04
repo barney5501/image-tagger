362dc3d0dcfd2274d931dd8d0121d8cb24f80368
Itay <Itaybn100@gmail.com>
2025-05-04 19:19:33 +0300

    basic db handling

--- src/app.py
+++ src/app.py
@@ -1,18 +1,29 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, g
 import os
+import sqlite3
 
 imagesPath = "static/images"
 imagesList = os.listdir(imagesPath)
 imagesList = [img for img in imagesList if not img.endswith(".txt")]
 imageIndex = 0
+DATABASE = "./tags.db"
 
 
 app = Flask(__name__)
 
 
+def get_db():
+    db = getattr(g, "_database", None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
 @app.route("/")
 def main():
-    return "Image Tagger!"
+    cur = get_db().cursor()
+    res = cur.execute("select * from tags;").fetchall()
+    return res
+    # return "Image Tagger!"
 
 
 @app.route("/tag")
@@ -43,3 +54,10 @@ def previousImage():
         return previous_image
     else:
         return jsonify(imagesList[imageIndex])
+
+
+@app.teardown_appcontext
+def close_connection(e):
+    db = getattr(g, "_database", None)
+    if db is not None:
+        db.close()
